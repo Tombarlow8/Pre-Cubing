990754c29e7c6895f05e6c0c4c25e7e52d57860a
Tom Barlow <TomBarlow8@gmail.com>
2020-02-15 12:49:35 +0000

    fixed pre-cubing error which was orphaning products
    introduced hazardous sopecific checks to the pre-cubign conditions so hazardous products don't get excluded.

--- Pre_Cubing.py
+++ Pre_Cubing.py
@@ -5,20 +5,21 @@ import pandas
 
 
 def load_products_from_csv(file_name):
-	product_and_QTY = []
 	data_frame = pandas.DataFrame(pandas.read_csv(file_name))
+	data_frame.sort_values(by=['haz_class'])
 	for row in data_frame.iterrows():
-		# [code, name, length, width, height, standard_qty, FP_qty, despatchable_flag,  haz_class, order_quantity]
-		my_product_array = [str(row[1][0]), str(row[1][1]), row[1][2], row[1][3], row[1][4], row[1][5], row[1][6], str(row[1][7]), str(row[1][8]), row[1][9]]
-		product_and_QTY.append(my_product_array)
-	return product_and_QTY
+		# [code, name, length, width, height, standard_qty, FP_qty, despatchable_flag,  haz_class]
+		my_product_array = [str(row[1][0]), str(row[1][1]), row[1][2], row[1][3], row[1][4], row[1][5], row[1][6], str(row[1][7]), str(row[1][8])]
+		create_products_from_csv_QTY(row, my_product_array)
 
-def create_products_from_csv_QTY(my_product_array): 
-	product_quantity = my_product_array[9]
-	product_standard_qty = my_product_array[5]
-	product_full_pallet_qty = my_product_array[6]
-	my_product = Pc.Product(*my_product_array[:9])
+def create_products_from_csv_QTY(row, my_product_array): 
+	product_quantity = row[1][9]
+	product_standard_qty = row[1][5]
+	product_full_pallet_qty = row[1][6]
+	my_product = Pc.Product(*my_product_array)
 	
+
+	# get full pallets
 	number_of_FP = (product_quantity - (product_quantity % product_full_pallet_qty)) // product_full_pallet_qty
 	
 	for i in range(product_quantity % product_full_pallet_qty % product_standard_qty):
@@ -30,6 +31,8 @@ def create_products_from_csv_QTY(my_product_array):
 			Full_pallet.FP_pack(my_product)
 		full_pallet_group.append(Full_pallet)
 
+
+	#get standards
 	if product_quantity % product_full_pallet_qty == 0:
 		number_of_standards = 0
 	else:
@@ -65,36 +68,54 @@ def check_hazards_in_group(aproduct, group):
         check_bool = False
 
     return check_bool
+
+def every_code_is_haz(group):
+    check_bool = True
+    if len(group) != 0:
+        for product in group:
+            if product.haz_class == "00":
+                check_bool = False
+
+    return check_bool
 	
    
 # TODO: condition not being met which exludes products from being added to a group     
 # add products into groups upto a limit
-def Create_box_group(count, current_group, groups_of_products, our_products, product, temp_vol):
-	if check_hazards_in_group(product, current_group) == False:
-	    # print(f"count {count} lengh: {len(our_products)}")
+def Create_box_group(current_group, groups_of_products, our_products, product, temp_vol):
+
+	haz_check = check_hazards_in_group(product, current_group)
+
+	if haz_check == False:
 	    temp_vol += product.volume
 	    # print(f"temp vol: {temp_vol} ")
-	    if temp_vol < box4vol and len(our_products) != count:
+	    if temp_vol < box4vol and len(our_products) != 1:
 	        current_group.append(product)
+	        our_products.remove(product)
 	    elif temp_vol > box4vol: #  and count != len(our_products):
 	        temp_vol -= temp_vol
 	        new_group = Bc.box_group()
 	        pack_and_clear(current_group, new_group, groups_of_products)
 	        # there will be a stray product which will need adding # DONE i think
 	        our_products.append(product)
-	    elif temp_vol < box4vol  and len(our_products) != count:
-	        temp_vol -= temp_vol
-	        new_group = Bc.box_group()
-	        pack_and_clear(current_group, new_group, groups_of_products)
-	    elif temp_vol < box4vol  and len(our_products) == count:
+	    elif temp_vol < box4vol  and len(our_products) == 1:
 	        temp_vol -= temp_vol
 	        new_group = Bc.box_group()
 	        current_group.append(product)
+	        our_products.remove(product)
 	        pack_and_clear(current_group, new_group, groups_of_products)
-	elif check_hazards_in_group(product, current_group) and temp_vol < box4vol  and len(our_products) == count:
+	elif haz_check and temp_vol < box4vol  and len(our_products) != 1 and every_code_is_haz(our_products):
 	    temp_vol -= temp_vol
 	    new_group = Bc.box_group()
 	    pack_and_clear(current_group, new_group, groups_of_products)
+	elif haz_check and temp_vol < box4vol  and len(our_products) == 1 and every_code_is_haz(our_products):
+	    temp_vol -= temp_vol
+	    new_group = Bc.box_group()
+	    pack_and_clear(current_group, new_group, groups_of_products)
+	    our_products.append(product)
+	elif haz_check and temp_vol < box4vol  and len(our_products) != 1:
+		temp_vol -= temp_vol
+		our_products.remove(product)
+		our_products.append(product)
 	else:
 	    somewhere_else.append(product)
 	return temp_vol
@@ -104,8 +125,8 @@ def group_products_box_groups(our_products):
     groups_of_products = []
     current_group = []  
     temp_vol = 0
-    for count, product in enumerate(our_products, start=1):  
-            temp_vol = Create_box_group(count, current_group, groups_of_products, our_products, product, temp_vol)
+    while len(our_products) != 0:
+            temp_vol = Create_box_group(current_group, groups_of_products, our_products, our_products[0], temp_vol)
     return groups_of_products
 
 
@@ -143,24 +164,18 @@ def pre_cubing_results():
 		print(f"box_type = {box.box_type}, box_vol = {box.get_contents_vol()}")
 		print(box.show_haz_list())
 		
-	# This is a temp funtion for debugging
-	# for box in my_boxes2:
-	# 	for products in box.contents:
-	# 		product_count += 1
-	# 	print(f"box_type = {box.box_type}, box_vol = {box.get_contents_vol()}")
-	# 	print(box.show_haz_list())
-
-
 	for standard in standard_group:
+		product_count += 1
 		print(f"product = {standard.product}, box_type = {standard.box_type}")
 
 	for full_pallet in full_pallet_group:
+		product_count += 1
 		print(f"product = {full_pallet.product}, box_type = {full_pallet.box_type}")
 
 	print(f"product count = {product_count}")
 
 	if len(somewhere_else)  > 0:
-		print("ERROR: There is an exception for the ore-cubing statements")
+		print("ERROR: There is an exception for the pre-cubing statements")
 
 
 if __name__ == "__main__":
@@ -175,11 +190,9 @@ if __name__ == "__main__":
 	box3vol = Bc.P3_box().volume
 	box4vol = Bc.P4_box().volume
 
-	# get the product master data from a csv file and then create the products as 'Product' Objects
-	order = load_products_from_csv(product_csv_file)
-	for line in order:
-		create_products_from_csv_QTY(line)
-
+	# get the product master data form a csv file and then create the products as 'Product' Objects
+	load_products_from_csv(product_csv_file)
+	
 	my_groups = group_products_box_groups(our_products)
 	my_boxes = pack_groups_into_into_boxes(my_groups)
 
